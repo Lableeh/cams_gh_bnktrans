f876c8554c3acbbdf18866b103302bf3ac88d2ea
Lovely Ruzanne Argente <Lovely.Ruzanne.Argente@ibm.com>
2019-06-27 16:16:29 +0800

    Initial commit

---
+++ cams_gh_bnktrans/src/main/java/com/ibm/cams/gh/onlinebank/cams_gh_bnktrans/CamsGhBnktransApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.cams.gh.onlinebank.cams_gh_bnktrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamsGhBnktransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamsGhBnktransApplication.class, args);
+	}
+
+}
